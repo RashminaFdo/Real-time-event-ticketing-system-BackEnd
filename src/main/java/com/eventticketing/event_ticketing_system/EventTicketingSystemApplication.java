f06c500c850a7c1a920e5746790efa43dd58bf72
Rashmina Fernando <fdo.rashmina@gmail.com>
2024-12-10 02:15:47 +0530

    created the initial files

---
+++ src/main/java/com/eventticketing/event_ticketing_system/EventTicketingSystemApplication.java
@@ -0,0 +1,15 @@
+package com.eventticketing.event_ticketing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketingSystemApplication.class, args);
+	}
+
+
+
+}
